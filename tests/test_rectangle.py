a69760df038a9df5bd534cb82e73bb4eeb4c757a
aum-barai-aiml-ak <aum.barai@armakuni.com>
2026-04-01 19:57:54 +0530

    abcd

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,11 +1,24 @@
-def test_area(my_rectangle):
+import pytest
+
+from source.shape import Rectangle
+
+
+def test_area(my_rectangle: Rectangle):
 
     assert my_rectangle.area() == 10 * 20
 
 
-def test_perimeter(my_rectangle):
+def test_perimeter(my_rectangle: Rectangle):
     assert my_rectangle.perimeter() == (10 * 2) + (20 * 2)
 
 
-def test_not_equal(my_rectangle, weird_rectangle):
+def test_not_equal(my_rectangle: Rectangle, weird_rectangle: Rectangle):
     assert my_rectangle == weird_rectangle
+
+
+def test_fail():
+    pytest.fail(
+        "deliberately failing for demo purpose, Lorem ipsum dolor sit amet, "
+        "consectetur adipiscing elit. Cras facilisis, massa in suscipit "
+        "dignissim, mauris lacus molestie nisi, quis varius metus nulla ut ipsum."
+    )
